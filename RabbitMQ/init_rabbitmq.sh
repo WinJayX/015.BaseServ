9319f7fd4344e14f4e67d6dabbb6cfec214f8c86
WinJay <WinJayX@Gmail.com>
2025-11-08 14:35:34 +0800

    fix: Modify compose  File .

--- RabbitMQ/init_rabbitmq.sh
+++ RabbitMQ/init_rabbitmq.sh
@@ -18,6 +18,11 @@ docker exec BaseServ-RabbitMQ3 /bin/bash -c 'rabbitmqctl reset'
 docker exec BaseServ-RabbitMQ3 /bin/bash -c 'rabbitmqctl join_cluster --ram rabbit@BaseServ-RabbitMQ1'
 docker exec BaseServ-RabbitMQ3 /bin/bash -c 'rabbitmqctl start_app'
 
+# 安装Stomp插件--前端消息实时提醒（消费者随机提醒，单一消费者）
+docker exec BaseServ-RabbitMQ1 /bin/bash -c 'rabbitmq-plugins enable rabbitmq_web_stomp rabbitmq_web_stomp_examples'
+docker exec BaseServ-RabbitMQ2 /bin/bash -c 'rabbitmq-plugins enable rabbitmq_web_stomp rabbitmq_web_stomp_examples'
+docker exec BaseServ-RabbitMQ3 /bin/bash -c 'rabbitmq-plugins enable rabbitmq_web_stomp rabbitmq_web_stomp_examples'
+
 #check cluster status
 echo "Check cluster status:"
 docker exec BaseServ-RabbitMQ1 /bin/bash -c 'rabbitmqctl cluster_status'
